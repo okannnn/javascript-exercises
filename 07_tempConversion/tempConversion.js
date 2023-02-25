5de8cffaf4003e070312359bbeae52d141e85bf8
okannnn <okanque@gmail.com>
2023-02-25 18:00:08 +0000

    Completed exercises 1-7 for fundamentals 4

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(val) {
+    cel = (val - 32) * 5/9
+    
+    
+    return +(Math.round(cel + "e+" + 1)  + "e-" + 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(val) {
+    fah = (val *9/5) + 32
+    
+    
+    return +(Math.round(fah + "e+" + 1)  + "e-" + 1);
 };
 
 // Do not edit below this line
